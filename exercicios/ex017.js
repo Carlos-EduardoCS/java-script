6910ec76c02f5bd76eac1bb7297c079562e57dc2
Carlos Eduardo <2005carloseduardosantos@gmail.com>
2023-06-01 14:45:33 -0300

    2023

---
+++ exercicios/ex017.js
@@ -0,0 +1,45 @@
+/* function parimpar(n) {
+    if (n%2 == 0 ){
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+
+let res = parimpar(77)
+
+console.log(res);  
+
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+
+console.log(soma(2, 5));
+let v = function (x) {
+    return x*2
+}
+
+console.log(v(5)); 
+
+
+function fatorial(n) {
+    let fat = 1
+    for (let c = n; c > 1; c--) {
+        fat *= c;
+    }
+    return fat
+}
+
+console.log(fatorial(5));*/
+
+
+// RECURSIVIDADE
+function fatorial(n) {
+    if (n == 1 ){
+        return 1
+    } else{
+        return n * fatorial(n-1)
+    }
+}
+
+console.log(fatorial(5));
